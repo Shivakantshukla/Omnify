8fd300edffd70644082a4051f3bbea838838d68a
Shivakantshukla <shiva_kant_shukla@outlook.com>
2025-04-12 20:07:49 +0530

    makemytrip hotel search flow

--- Omnify/src/main/java/pages/HotelBookingPage.java
+++ Omnify/src/main/java/pages/HotelBookingPage.java
@@ -3,12 +3,14 @@ package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
+
 public class HotelBookingPage extends BasePage {
 
     public HotelBookingPage(WebDriver driver) {
         super(driver);
     }
-
+    //closing the login popup page
     public void closeLoginPopup() {
         try {
             WebElement body = driver.findElement(By.tagName("body"));
@@ -18,16 +20,45 @@ public class HotelBookingPage extends BasePage {
 
     public void clickHotelsTab() {
         click(By.xpath("//span[text()='Hotels']"));
+        // going to hotel booking page
     }
 
-    public void enterCity(String cityName) {
-        click(By.id("city"));
-        click(By.xpath("//p[contains(text(),'" + cityName + "')]"));
+    public void enterCity(String cityName) throws InterruptedException {
+//        WebElement cityInput = driver.findElement(By.id("city"));
+//        cityInput.click();
+//        click(By.xpath("//input[@id='city']"));
+//        WebElement inputField = driver.findElement(By.xpath("//input[@id=\"city\"]"));
+//        inputField.sendKeys("Bangalore");
+        // Click the city field first to activate the input
+        WebElement cityContainer = driver.findElement(By.xpath("//input[@id=\"city\"]"));
+        cityContainer.click();
+
+        Thread.sleep(2000); // Wait for the input to become interactable
+
+// Locate the actual input field and type "Bangalore"
+        WebElement inputField = driver.findElement(By.xpath("//input[@placeholder='Where do you want to stay?']"));
+        inputField.sendKeys("Bangalore");
+
+        Thread.sleep(2000); // Wait for dropdown options to appear
+
+// Select the first suggestion
+//        WebElement firstSuggestion = driver.findElement(By.xpath("//p[contains(text(), 'Bangalore')]"));
+//        firstSuggestion.click();
+        click(By.xpath("//div[@class='searchedResult font16 darkText']"));
+
     }
 
     public void selectDates(String checkInLabel, String checkOutLabel) {
+//        click(By.xpath("//input[@id='checkin']"));
         click(By.xpath("//div[@aria-label='" + checkInLabel + "']"));
+        //accpet date as Thu Apr 17 2025 format
+//        click(By.xpath("//input[@id='checkout']"));
         click(By.xpath("//div[@aria-label='" + checkOutLabel + "']"));
+    }
+
+    public void selectGuest(){
+        //selecting prefilled guest count
+        click(By.xpath("//button[@class='primaryBtn btnApplyNew pushRight capText']"));
     }
 
     public void clickSearch() {
@@ -35,6 +66,12 @@ public class HotelBookingPage extends BasePage {
     }
 
     public void selectFirstHotel() {
+
+
+
+
+
+
         waitForVisible(By.cssSelector("div.makeFlex"));
         click(By.cssSelector("div.makeFlex a"));
 
@@ -43,6 +80,10 @@ public class HotelBookingPage extends BasePage {
             driver.switchTo().window(winHandle);
         }
         waitForVisible(By.tagName("body"));
+
+//        List<WebElement> element = driver.findElements(By.xpath("//div[@class='flexOne makeFlex']"));
+//        element.click();
+
     }
 
     public void clickBookNow() {
@@ -51,11 +92,23 @@ public class HotelBookingPage extends BasePage {
 
     // Simulate or locate a discount label
     public boolean verifyDiscountApplied() {
-        try {
-            WebElement discountLabel = driver.findElement(By.xpath("//*[contains(text(), 'discount') or contains(text(), 'OFF')]"));
-            return discountLabel.isDisplayed();
-        } catch (NoSuchElementException e) {
+        try{
+            //chceking if coupon is selected and discount is applied
+            WebElement discountApplied = driver.findElement(By.xpath("//p[@class='font12 lineHight16']"));
+            if(discountApplied.getText().contains("Congratulations! Discount of ")){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (NoSuchElementException e) {
             return false;
         }
+    }
+
+
+    public void applyCouponCode(String Code){
+        WebElement enterCoupon = driver.findElement(By.xpath("//input[@placeholder='Have a Coupon Code']"));
+        enterCoupon.sendKeys(Code);
+        click(By.xpath("//a[@data-testid='applyCpnBtn']"));
     }
 }
--- Omnify/src/main/java/tests/HotelBookingTest.java
+++ Omnify/src/main/java/tests/HotelBookingTest.java
@@ -5,45 +5,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import pages.BookingPage;
+import pages.HotelBookingPage;
 
 public class HotelBookingTest {
 
     WebDriver driver;
-    BookingPage bookingPage;
+    HotelBookingPage bookingPage;
 
     @BeforeClass
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://your-hotel-app.com"); // Replace with actual app URL
-        bookingPage = new BookingPage(driver);
+        driver.get("https://www.makemytrip.com/");
+        bookingPage = new HotelBookingPage(driver);
     }
 
     @Test
-    public void testHotelBookingWithCoupon() {
-        bookingPage.searchHotels("New York", "2025-04-10", "2025-04-15");
-
+    public void bookHotelWithCouponTest() throws InterruptedException {
+        bookingPage.closeLoginPopup();
+        bookingPage.clickHotelsTab();
+        Thread.sleep(3000);
+        bookingPage.enterCity("New York");
+        Thread.sleep(3000);
+        bookingPage.selectDates("Sun Apr 20 2025", "Mon Apr 21 2025");
+//        Thread.sleep(3000);
+        bookingPage.selectGuest();
+        bookingPage.clickSearch();
+                Thread.sleep(6000);
         bookingPage.selectFirstHotel();
-
-        bookingPage.applyCoupon("SUMMER25");
-
-        String discount = bookingPage.getDiscountText();
-        String finalPrice = bookingPage.getFinalPrice();
-
-        System.out.println("Discount: " + discount);
-        System.out.println("Final Price: " + finalPrice);
-
-        Assert.assertTrue(discount.contains("25") || discount.length() > 0, "Discount not applied");
-
-        bookingPage.proceedToCheckout();
-
-        // Do NOT complete payment
+        bookingPage.clickBookNow();
+        bookingPage.applyCouponCode("SUMMER25");
+        // Simulate coupon applied check
+        boolean discountApplied = bookingPage.verifyDiscountApplied();
+        Assert.assertTrue(discountApplied, "Sorry! Promocode used is not applicable at the moment.");
     }
 
+
+
     @AfterClass
-    public void tearDown() {
+    public void teardown() {
         driver.quit();
     }
 }
